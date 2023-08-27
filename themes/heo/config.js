59b560cb97d7f65609d316c164896cacafb18d1a
ewili <37134944+ewili@users.noreply.github.com>
2023-08-27 23:41:15 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,12 +10,12 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  //HERO_TITLE_1: '分享编程',
-  //HERO_TITLE_2: '与思维认知',
-  //HERO_TITLE_3: 'TANGLY1024.COM',
-  //HERO_TITLE_4: '新版上线',
-  //HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  //HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: '亿威力',
+  HERO_TITLE_2: '无限连结',
+  HERO_TITLE_3: 'ewili.com',
+  HERO_TITLE_4: '亿威力',
+  HERO_TITLE_5: '无限连结',
+  HERO_TITLE_LINK: 'https://www.ewili.com',
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
